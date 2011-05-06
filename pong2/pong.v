423f89d0cbb3d38caeb181bdec6e71dc4fe83d29
Ali Raheem <Ali.Raheem@gmail.com>
2011-05-06 13:04:11 +0100

    Heavily commented and re jigged

--- pong2/pong.v
+++ pong2/pong.v
@@ -5,9 +5,11 @@ module pong(CLOCK_50, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, SW, KEY, HEX3, HEX2, 
 	input [3:0] KEY;
 	output [3:0] VGA_R, VGA_G, VGA_B;
 	output VGA_HS, VGA_VS;
+//Generate 25MHz clock for TV
 	reg clk,clk5;
 	always@(posedge CLOCK_50)
 		clk <= ~clk;
+//Very slow clock for movement originally intended to be 5Hz but I just changed it until it looked good on screen
 	reg [31:0] counter;
 	always@(posedge clk)
 		if(counter == 50000) begin
@@ -15,33 +17,32 @@ module pong(CLOCK_50, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, SW, KEY, HEX3, HEX2, 
 			clk5 <= ~clk5;
 		end else
 			counter <= counter + 1;
+//Syncs with the monitor and provides us with x,y and when the display is active
 	wire [9:0] x, y;
 	vga_sync (.iclk(clk), .oVGA_HS(VGA_HS), .oVGA_VS(VGA_VS), .oActive(display), .oX(x), .oY(y));
-	
+//The ball it's actually a square 10:13 cause ratio is funny	
 	wire [3:0] ball;
 	reg [9:0] ballX, ballY;
 	assign ball = ((x>ballX) && (x<ballX+10) && (y>ballY) && (y<ballY+13))?4'b1111:4'b0;
-
+//Player one paddle
 	wire [3:0] paddle;
 	reg [9:0] paddleY;
 	assign paddle = ((y>paddleY) && (y<paddleY+50) && (x>10) && (x<25))?4'b1111:4'b0;
-
-	wire [3:0] paddle1;
-	reg [9:0] paddleY1;
-	assign paddle1 = ((y>paddleY1) && (y<paddleY1+50) && (x>615) && (x<630))?4'b1111:4'b0;
-
 	always@(posedge clk5)
 		if(~KEY[2] && paddleY<430)
 			paddleY <= paddleY + 1;
 		else if (~KEY[3] && |paddleY)
 			paddleY <= paddleY - 1;
-			
+//Player two paddle	
+	wire [3:0] paddle1;
+	reg [9:0] paddleY1;
+	assign paddle1 = ((y>paddleY1) && (y<paddleY1+50) && (x>615) && (x<630))?4'b1111:4'b0;		
 	always@(posedge clk5)
 		if(~KEY[0] && paddleY1<430)
 			paddleY1 <= paddleY1 + 1;
 		else if (~KEY[1] && |paddleY1)
 			paddleY1 <= paddleY1 - 1;
-			
+//v = velocity determines movment in x/y fliped in a bounce
 	reg [1:0] v;
 	always@(posedge clk5)
 		if(~miss) begin
@@ -51,15 +52,9 @@ module pong(CLOCK_50, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, SW, KEY, HEX3, HEX2, 
 			ballX <= 320;
 			ballY <= 240;
 		end
-	wire [7:0] wscore, wscore1;
-	bcd (.in(score), .out(wscore));
-	bcd (.in(score1), .out(wscore1));
-	b2seg (.in(wscore[7:4]), .out(HEX3));
-	b2seg (.in(wscore[3:0]), .out(HEX2));
-	b2seg (.in(wscore1[7:4]), .out(HEX1));
-	b2seg (.in(wscore1[3:0]), .out(HEX0));
+//Detect bounce and sound a miss
 	reg bounceX, bounceY, miss;
-	always@(posedge clk)
+	always@(posedge CLOCK_50)
 		if(ballY == 0 && ~bounceY) begin
 			v[1] <= 0;
 			bounceY <= 1;
@@ -89,11 +84,22 @@ module pong(CLOCK_50, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, SW, KEY, HEX3, HEX2, 
 			bounceX <= 0;
 			miss <= 0;
 		end
+//central white line could also make border with this.
 	wire [3:0] line;
 	assign line = (x>318 && x<322 && y>10 && y<470)?4'b1111:4'b0;
+//Display score on 7 seg	
+	wire [7:0] wscore, wscore1;
+	bcd (.in(score), .out(wscore));
+	bcd (.in(score1), .out(wscore1));
+	b2seg (.in(wscore[7:4]), .out(HEX3));
+	b2seg (.in(wscore[3:0]), .out(HEX2));
+	b2seg (.in(wscore1[7:4]), .out(HEX1));
+	b2seg (.in(wscore1[3:0]), .out(HEX0));
+//Send VGA signals white = 1111 which always wins in |	
 	assign VGA_R = display?{SW[8:6],1'b0}|line|ball|paddle|paddle1:0;
 	assign VGA_G = display?{SW[5:3],1'b0}|line|ball|paddle|paddle1:0;
 	assign VGA_B = display?{SW[2:0],1'b0}|line|ball|paddle|paddle1:0;
+//Not needed but makes it a little nicer
 	initial begin
 		ballX = 320;
 		ballY = 240;
