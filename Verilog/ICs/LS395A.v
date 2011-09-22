4700d7aa0e0db1a8143bdbadff6e98a2cf80007d
Ali Raheem <AliRaheem@LetsHackStuff.com>
2011-09-23 00:58:23 +0100

    Added LS395A IC

---
+++ Verilog/ICs/LS395A.v
@@ -0,0 +1,26 @@
+module icLS395A(iClk, iClr, iOC, iLDSH, iSER, iData, oData, oCarry);
+input iClk;
+input iClr;
+input iOC;
+input iLDSH;
+input iSER;
+input [3:0] iData;
+output [3:0] oData;
+output oCarry;
+
+reg [3:0] Data;
+
+always@(negedge iClk) begin
+	if(iClr)
+		Data = 4'b0000;
+	else if(iLDSH)
+		Data = iData;
+	else begin
+		Data <= Data<<1;
+		Data[0] <= iSER;
+	end
+end
+assign oData = iOC? 4'bxxxx : Data;
+assign oCarry = Data[3];
+endmodule
+
